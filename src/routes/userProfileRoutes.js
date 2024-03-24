99f6299ab3ae1b40ad3870beeafa91ba31d6bbd8
kanpech-t <kankasem3002@gmail.com>
2024-03-24 14:15:54 +0700

    add change information and sent report

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -3,12 +3,26 @@ require("dotenv").config();
 const express = require("express");
 
 const User = require("../models/User.js");
+const Report = require("../models/Report.js");
 
 const { hashPassword, comparePasswords } = require("../utils/function.js");
 
 const router = express.Router();
 
-// router.put("/information");
+const multer = require("multer");
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+
+const { getStorage, getDownloadURL } = require("firebase-admin/storage");
+
+const bucket = getStorage().bucket();
+
+const crypto = require("crypto");
+const path = require("path");
+
+const { v4: uuidv4 } = require("uuid");
 
 router.put("/password", async (req, res) => {
   try {
@@ -31,6 +45,70 @@ router.put("/password", async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(404).json({ error: err });
+  }
+});
+
+router.put("/information", upload.single("profileImg"), async (req, res) => {
+  try {
+    const profileImg = req.file;
+    const userId = req.user.id;
+    const user = await User.findOne({ id: userId });
+    const { username, email, gender } = req.body;
+    if (profileImg) {
+      const randomString = crypto.randomBytes(12).toString("hex");
+      const fileName =
+        Date.now() + "_" + randomString + path.extname(profileImg.originalname);
+      const file = bucket.file(fileName);
+
+      const stream = file.createWriteStream().end(profileImg.buffer);
+
+      const result = await new Promise((resolve, reject) => {
+        stream.on("finish", async () => {
+          try {
+            const downloadURL = await getDownloadURL(file);
+            resolve({ url: downloadURL, fileName: fileName });
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+      user.profileUrl = result.url;
+    }
+    if (username) {
+      user.username = username;
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (gender) {
+      user.gender = gender;
+    }
+    await user.save();
+
+    return res.json("success change information");
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/report", async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { detail } = req.body;
+
+    if (!detail) {
+      return res.status(404).json("detail not found");
+    }
+
+    await Report.create({
+      reportId: uuidv4(),
+      createBy: userId,
+      detail: detail,
+    });
+
+    return res.json("report sent");
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
   }
 });
 
